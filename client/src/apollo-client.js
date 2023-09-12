7a00c52236e67d8e16ae7ec85527ad1c23221c66
jonny desktop <jon@uslms.com>
2023-09-11 21:23:52 -0600

    changed the client

--- client/src/apollo-client.js
+++ client/src/apollo-client.js
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-const HTTP_URI = "http://localhost:5500/graphql";
+const HTTP_URI =
+  process.env.NODE_ENV === "production"
+    ? "https://memory-map-1fd827e00c4d.herokuapp.com/graphql"
+    : "http://localhost:5500/graphql";
 
 const httpLink = new HttpLink({
   uri: HTTP_URI,
